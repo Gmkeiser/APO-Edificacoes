9e87e6daddb42494f1feae4d466e52a2e560f9a7
Edison Matias <devedisonmatias@gmail.com>
2022-09-18 14:05:07 -0300

    Add remaining pages

---
+++ client/src/Login.vue
@@ -0,0 +1,62 @@
+<template>
+  <header>
+    <h1>Faça seu login</h1>
+  </header>
+  <form method="post" action="/loginfunc.jar">
+    <input id="email" type="email" placeholder="E-mail"><br><br>
+    <input id="senha" type="password" placeholder="Senha"><br><br>
+    <a href="esqueceu_senha" id="esqueceusenha">Esqueceu a senha?</a><br><br>
+    <input type="submit" value="Login" id="botao_login">
+    <br><br>
+  </form>
+  <footer id="rodape">
+    <label id="cdt">Ainda não cadastrado? <a href="cadastrar_usuario.html">Cadastre-se aqui!</a></label>
+  </footer>
+</template>
+
+<style>
+#email, #senha{
+    font-size: 2vh;
+    height: fit-content;
+    width: 100%;
+    border-top: none;
+    border-left: none;
+    border-right: none;
+}
+
+form {
+    position: absolute;
+    left: 50% ;
+    top : 50% ;
+    transform: translate(-50%,-50%);
+    text-align: center;
+
+
+}
+
+footer {
+    position: absolute;
+    left: 50% ;
+    top :80%;
+    transform: translate(-50%,-80%);
+    text-align: center;
+
+}
+
+header {
+    position: absolute;
+    left: 50% ;
+    top: 20%;
+    transform: translate(-50%,-20%);
+    text-align:center;
+    font-size: 1.9vh;
+}
+
+#botao_login 
+{
+    padding-inline: 6vh;
+    border-radius: 30px;
+    border: none;
+    background-color: yellow;
+}
+</style>
---
+++ client/src/PaginaInicial.vue
@@ -0,0 +1,96 @@
+<template>
+  <div id="container">
+    <header>
+      <h1>Prefeitura de Jambu</h1>
+    </header>
+    <main>
+      <div class="box">
+        <div class="icon"></div>
+        <div class="text">
+          <h2>Cadastrar Nova Escola</h2>
+          <p>Para primeira avaliação cadastrar escola</p>
+        </div>
+      </div>
+      <div class="box">
+        <div class="icon"></div>
+        <div class="text">
+          <h2>Avaliação de Escola</h2>
+        </div>
+      </div>
+    </main>
+    <button>Meu Perfil</button>
+  </div>
+</template>
+
+<style>
+* {
+  margin: 0;
+}
+
+#container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+}
+
+
+header {
+  background-color: orange;
+  padding: 2rem;
+  height: 10%;
+}
+
+h1 {
+  font-size: 1.5rem;
+  text-align: right;
+}
+
+main {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+}
+
+button {
+  margin: 1.5rem;
+  font-size: 1.25rem;
+  font-weight: bold;
+  background-color: orange;
+  border: none;
+  padding: 0.75rem 0;
+  font-family: serif;
+  border-radius: 10px;
+}
+
+.box {
+  background-color: #1e78ff;
+  margin: 1.5rem;
+  height: 150px;
+  display: grid;
+  grid-template-columns: 25% 75%;
+  width: 100%;
+  max-width: 600px;
+}
+
+.text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: 1.5rem;
+}
+
+.icon {
+  background-color: #bd2323;
+}
+
+h2,
+p {
+  color: white;
+  text-align: center;
+}
+</style>
