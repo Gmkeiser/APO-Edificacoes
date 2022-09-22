41cac33caa2d3e18248c803074ed157dceb3d55b
Edison Matias <devedisonmatias@gmail.com>
2022-09-22 16:39:58 -0300

    refactored login page

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -1,10 +1,20 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex column flex-center justify-evenly">
     <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+      alt="Jambu"
+      src="~assets/brasao.svg"
     >
+    <h1 class="text-h4">Faça seu login</h1>
+    <div>
+      <q-input label="E-mail" style="width: 300px;"/>
+      <q-input label="Senha" style="width: 300px;"/>
+    </div>
+    <a href="">Esqueci minha senha</a>
+    <q-btn unelevated rounded label="Login" color="amber" style="width: 150px;"/>
+    <div>
+      <span>Ainda não tem uma conta? </span>
+      <a href="">Cadastre-se</a>
+    </div>
   </q-page>
 </template>
 
